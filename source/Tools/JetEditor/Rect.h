ddece14d600374684f06f015c09dc0935b25b9c4
paradoxnj <paradoxnj@comcast.net>
2016-01-13 20:04:39 -0500

    Editor 2D views...correct copyright notices.

--- source/Tools/JetEditor/Rect.h
+++ source/Tools/JetEditor/Rect.h
@@ -1,23 +1,24 @@
-/****************************************************************************************/
-/*  RECT.H                                                                              */
-/*                                                                                      */
-/*  Author:                                                                             */
-/*  Description:                                                                        */
-/*                                                                                      */
-/*  The contents of this file are subject to the Jet3D Public License                   */
-/*  Version 1.02 (the "License"); you may not use this file except in                   */
-/*  compliance with the License. You may obtain a copy of the License at                */
-/*  http://www.jet3d.com                                                                */
-/*                                                                                      */
-/*  Software distributed under the License is distributed on an "AS IS"                 */
-/*  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See                */
-/*  the License for the specific language governing rights and limitations              */
-/*  under the License.                                                                  */
-/*                                                                                      */
-/*  The Original Code is Jet3D, released December 12, 1999.                             */
-/*  Copyright (C) 1996-1999 Eclipse Entertainment, L.L.C. All Rights Reserved           */
-/*                                                                                      */
-/****************************************************************************************/
+/*
+	@file Rect.h
+	@author paradoxnj
+	@brief Rectangle wrapper
+
+	@par license
+	The contents of this file are subject to the Jet3D Public License
+	Version 1.02 (the "License"); you may not use this file except in
+	compliance with the License. You may obtain a copy of the License at
+	http://www.jet3d.com
+
+	@par
+	Software distributed under the License is distributed on an "AS IS"
+	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+	the License for the specific language governing rights and limitations
+	under the License.
+
+	@par
+	The Original Code is Jet3D, released December 12, 1999.
+	Copyright (C) 1996-1999 Eclipse Entertainment, L.L.C. All Rights Reserved
+*/
 #pragma once
 
 // Disable inline functions removed warning
--- source/Tools/JetEditor/defs.h
+++ source/Tools/JetEditor/defs.h
@@ -1,33 +1,28 @@
-/****************************************************************************************/
-/*  DEFS.H                                                                              */
-/*                                                                                      */
-/*  Author:                                                                             */
-/*  Description:                                                                        */
-/*                                                                                      */
-/*  The contents of this file are subject to the Jet3D Public License                   */
-/*  Version 1.02 (the "License"); you may not use this file except in                   */
-/*  compliance with the License. You may obtain a copy of the License at                */
-/*  http://www.jet3d.com                                                                */
-/*                                                                                      */
-/*  Software distributed under the License is distributed on an "AS IS"                 */
-/*  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See                */
-/*  the License for the specific language governing rights and limitations              */
-/*  under the License.                                                                  */
-/*                                                                                      */
-/*  The Original Code is Jet3D, released December 12, 1999.                             */
-/*  Copyright (C) 1996-1999 Eclipse Entertainment, L.L.C. All Rights Reserved           */
-/*                                                                                      */
-/****************************************************************************************/
+/*
+	@file defs.h
+	@author paradoxnj
+	@brief Defines and constants
+
+	@par license
+	The contents of this file are subject to the Jet3D Public License
+	Version 1.02 (the "License"); you may not use this file except in
+	compliance with the License. You may obtain a copy of the License at
+	http://www.jet3d.com
+
+	@par
+	Software distributed under the License is distributed on an "AS IS"
+	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+	the License for the specific language governing rights and limitations
+	under the License.
+
+	@par
+	The Original Code is Jet3D, released December 12, 1999.
+	Copyright (C) 1996-1999 Eclipse Entertainment, L.L.C. All Rights Reserved
+*/
 #pragma once
-
-// This module is a catch-all for defines
-
-#ifndef DEFS_H
-#define DEFS_H
 
 #define HALFHANDLESIZE	(3)
 #define HANDLESIZE		(6)
-
 
 typedef enum OBJECT_KIND
 {
@@ -144,7 +139,3 @@ typedef enum MODE
 	MODE_POINTER_ARCH,
 	MODE_LAST
 } MODE ;
-
-
-#endif // Prevent multiple inclusion
-/* EOF: Defs.h */
