67937ac85e2ed5b16dc52935a418006e0b9a5e9d
paradoxnj <paradoxnj@comcast.net>
2016-11-03 17:09:10 -0400

    C++ Conversion, code cleanup

---
+++ include/jeVec3d.h
@@ -0,0 +1,56 @@
+/*!
+	@file jeVec3d.h
+
+	@author Anthony Rufrano (paradoxnj)
+	@brief 3D Vector math
+
+	@par License
+	The contents of this file are subject to the Jet3D Public License
+	Version 1.02 (the "License"); you may not use this file except in
+	compliance with the License. You may obtain a copy of the License at
+	http://www.jet3d.com
+
+	@par
+	Software distributed under the License is distributed on an "AS IS"
+	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+	the License for the specific language governing rights and limitations
+	under the License.
+
+	@par
+	The Original Code is Jet3D, released December 12, 1999.
+	Copyright (C) 1996-1999 Eclipse Entertainment, L.L.C. All Rights Reserved
+*/
+#ifndef __JE_VEC3D_H__
+#define __JE_VEC3D_H__
+
+#include "Basetype.h"
+
+namespace jet3d {
+
+struct JETAPI jeVec3d
+{
+	jeVec3d();
+	jeVec3d(jeFloat _x, jeFloat _y, jeFloat _z);
+	virtual ~jeVec3d();
+
+	jeFloat x, y, z;
+
+	static jeVec3d UP;
+	static jeVec3d DOWN;
+	static jeVec3d LEFT;
+	static jeVec3d RIGHT;
+	static jeVec3d ZERO;
+
+	static jeVec3d X_AXIS;
+	static jeVec3d Y_AXIS;
+	static jeVec3d Z_AXIS;
+
+	jeVec3d &add(const jeVec3d &v1);
+	jeVec3d &subtract(const jeVec3d &v1);
+	jeVec3d &inverse();
+
+};
+
+} // namespace jet3d
+
+#endif
